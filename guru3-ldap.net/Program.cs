89d1cc4bd1f16c31a3e517a3acb6d219dae77f97
Bianco Veigel <zivillian@eventphone.de>
2018-10-27 20:41:29 +0200

    Projektdateien hinzufügen.

---
+++ guru3-ldap.net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace guru3_ldap.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

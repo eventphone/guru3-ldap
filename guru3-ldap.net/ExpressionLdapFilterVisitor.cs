9fb5ddd5dbc8ec5c109aa0571539accee3dec6fe
Bianco Veigel <zivillian@eventphone.de>
2018-11-01 20:35:31 +0100

    support more ldap filter

--- guru3-ldap.net/ExpressionLdapFilterVisitor.cs
+++ guru3-ldap.net/ExpressionLdapFilterVisitor.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq.Expressions;
-using zivillian.ldap.Attributes;
 
 namespace eventphone.guru3.ldap
 {
@@ -9,11 +8,16 @@ namespace eventphone.guru3.ldap
         {
             switch (name.ToLowerInvariant())
             {
-                case"cn":
+                case "cn":
+                case "2.5.4.3":
+                case "telephonenumber":
+                case "2.5.4.20":
                     return Expression.Property(ExpressionParameter, typeof(LdapExtension).GetProperty(nameof(LdapExtension.Number)));
-                case"sn":
+                case "sn":
+                case "2.5.4.4":
                     return Expression.Property(ExpressionParameter, typeof(LdapExtension).GetProperty(nameof(LdapExtension.Name)));
-                case"l":
+                case "l":
+                case "2.5.4.7":
                     return Expression.Property(ExpressionParameter, typeof(LdapExtension).GetProperty(nameof(LdapExtension.Location)));
                 default:
                     return null;

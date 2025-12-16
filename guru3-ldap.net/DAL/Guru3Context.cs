c83dc84452f7bd2c12c677a59669f42adefa7c60
Bianco Veigel <bianco@unicorn-development.de>
2025-12-16 14:14:29 +0100

    switch to postgres

--- guru3-ldap.net/DAL/Guru3Context.cs
+++ guru3-ldap.net/DAL/Guru3Context.cs
@@ -23,7 +23,7 @@ namespace eventphone.guru3.ldap.DAL
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString));
+                optionsBuilder.UseNpgsql(_connectionString);
             }
         }
 
